82e2550471e28177d51b83180951b3ab64903c61
AnaLung <ana.lung2003@yahoo.com>
2020-11-04 17:04:38 +0200

    lectie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapAna.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapAna.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
+
 public class HardwareMapAna {
+
+    // intreg a,b;
+    /**  assddffg */
+    public DcMotor right = null;
+    public DcMotor left = null;
+
+    public Servo brat = null;
+
+    HardwareMap HardwareMapAna = null;
+
+    public void init (HardwareMap ahwMap)
+    {
+        HardwareMapAna = ahwMap;
+
+        right = HardwareMapAna.get(DcMotor.class, "dreapta");
+        left =  HardwareMapAna.get(DcMotor.class, "stanga");
+
+        brat = HardwareMapAna.get(DcMotor.class, "brat");
+
+        right.setDirection(DcMotor.Direction.REVERSE);
+        left.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
 }
