11fccc8decd457917395028128af34d22ee3ad3e
NataliaBarz <73419366+NataliaBarz@users.noreply.github.com>
2020-11-04 17:07:44 +0200

    Proiect_nou_hardware_map tema

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NataliaBarz_hardwaremap2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NataliaBarz_hardwaremap2.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-public class NataliaBarz_hardwaremap2 {"hardwaremap"
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class NataliaBarz_hardwaremap2 {
+    public DcMotor right = null;
+    public DcMotor left = null;
+
+    public Servo brat = null;
+
+    NataliaBarz_hardwaremap2 = null;
+    public void init {HardwareMap ahwMap}
+    {
+        HardwareMap = ahwMap;
+        right = NataliaBarz_hardwaremap2.get(DcMotor.class, deviceName"right");
+        left = NataliaBarz_hardwaremap2.get(DcMotor.class, deviceName"left");
+
+                brat = NataliaBarz_hardwaremap2.get(DcMotor.class, device"brat");
+                        right.setDirection(DcMotor.Direction.REVERSE);
+                        left.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+
 }
