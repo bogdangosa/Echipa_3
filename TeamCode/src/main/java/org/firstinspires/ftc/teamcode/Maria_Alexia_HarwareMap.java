38d19ce1134a448236bce9b80f8dc8ceb3bd4ccd
Prunaa <73481945+Prunaa@users.noreply.github.com>
2020-11-05 19:07:19 +0200

    Update Maria_Alexia_HarwareMap.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Maria_Alexia_HarwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Maria_Alexia_HarwareMap.java
@@ -19,10 +19,16 @@ public class Maria_Alexia_HarwareMap {
         right = Maria_Alexia_HardwareMap.get(DcMotor.class,"right");
         left = Maria_Alexia_HardwareMap.get(DcMotor.class,"left");
 
-        brat = Maria_Alexia_HardwareMap.get(DcMotor.class,"brat");
+        brat = Maria_Alexia_HardwareMap.get(Servo.class,"brat");
 
         right.setDirection(DcMotor.Direction.REVERSE);
         left.setDirection(DcMotor.Direction.FORWARD);
+
+        right.setPower(0);
+        left.setPower(0);
+
+        brat.setPosition(0);
+        
 
 
     }
